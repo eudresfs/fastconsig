5d3eb1b2d5fdee966c79847a6ee202e0431a3580
Eudres Freitas <eudres.freitas@hotmail.com>
2026-01-15 09:31:01 -0300

    chore: cleanup remaining product development artifacts
    - Remove legacy feature documentation
    - Delete remaining scaffolding packages
    - Clean up old configuration files

--- product-development/scaffolding/packages/ui/src/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export { useMediaQuery } from './use-media-query'
-export { useDebounce } from './use-debounce'
-export { useLocalStorage } from './use-local-storage'
-export { useToast } from './use-toast'
